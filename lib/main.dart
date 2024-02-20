46dc7a1111afd303864625288035196be6d4ff22
Yash dubey <dubeyyash.2003@gmail.com>
2024-02-20 09:56:08 +0530

    change

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialAppp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Homescreen(),
     );
